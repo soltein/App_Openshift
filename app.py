b5234aa371b17de1178d29846a981188ad545aa6
paulovigne <57726813+paulovigne@users.noreply.github.com>
2022-03-08 13:23:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Bem Vindo!"
+    return "Bem Vindo!!!"
 
 @app.route('/comovai')
 def hello():
-    return 'Melhor Agora!'
+    return 'Melhor Agora!!!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
