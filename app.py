0d692f5f0ca031009e6933a1fec75065e3de1912
paulovigne <57726813+paulovigne@users.noreply.github.com>
2022-11-24 17:00:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Bem Vindo Alunos!!!"
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Hello World</p></h1>'
 
-@app.route('/comovai')
+@app.route('/api')
 def hello():
-    return 'Melhor Agora!!!'
+    return '{status: ok}'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
