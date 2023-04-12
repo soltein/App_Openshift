4d30efc5328bc55e9f5b5e929c3df47acb3d5abb
Julio Cezar <julio@soltein.com.br>
2023-04-12 12:07:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Trabalho Infnet OpenShift</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Trabalho Infnet OpenShift Teste2</p></h1>'
 
 @app.route('/api')
 def hello():
